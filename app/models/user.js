7c744b0128c0d00bf1830fe9d9fa37a989adcbc0
Ivan Nosov <i.k.nosov@gmail.com>
2015-04-25 19:03:53 +0300

    user model

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName'),
+  email: DS.attr('string'),
+  login: DS.attr('string'),
+  created: DS.attr('date'),
+  updated: DS.attr('date')
+});
--- server/mocks/users.js
+++ server/mocks/users.js
@@ -1,7 +1,7 @@
 
 var fixtures = [
-  { id: 1, firstName: 'John', lastName: 'Doe' },
-  { id: 2, firstName: 'Jane', lastName: 'Doe' }
+  { id: 1, firstName: 'John', lastName: 'Doe', email: 'john.doe@mail.com', login: 'john', created: new Date(), updated: new Date() },
+  { id: 2, firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@mail.com', login: 'jane', created: new Date(), updated: new Date() }
 ];
 
 module.exports = function(app) {
