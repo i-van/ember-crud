ccd815bafdf96c7c57cb763b2cc350866530d790
Ivan Nosov <i.k.nosov@gmail.com>
2015-05-02 23:25:00 +0300

    rename modal-dialog component to x-modal

--- app/components/form-input.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  classNames: ['form-group'],
-  classNameBindings: ['hasError:has-error'],
-  hasError: function() {
-    return this.get('errors');
-  }.property('errors')
-});
